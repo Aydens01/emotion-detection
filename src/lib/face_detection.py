df9527869691df7787d6c14377f0630c8a7b6ee6
lafageadri <lafageadri@eisti.eu>
2020-01-06 16:11:12 +0200

    Add comment in face_detection.py file

--- src/lib/face_detection.py
+++ src/lib/face_detection.py
@@ -4,6 +4,7 @@ import numpy as np
 from skimage import transform
 from torchvision.transforms import functional as F
 
+# TO CHANGE ACCORDINGLY
 PATH = '/home/eisti/Private/Coding/emotion-prediction/src/lib/'
 
 class FaceDetection:
